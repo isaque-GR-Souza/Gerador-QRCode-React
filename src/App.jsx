b6c55feb4937a9945c1bc949b06664d9ec4c90d6
Isaque Gonçalves Rodrigues de Souza <112179402+isaque-GR-Souza@users.noreply.github.com>
2022-11-15 02:38:49 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import QRCode from 'qrcode'
+import { useState } from 'react'
+
+function App() {
+  const [url, setUrl] = useState('')
+  const [qrcode, setQrcode] = useState('')
+
+  const GeradorQRcode = () => {
+    QRCode.toDataURL(url, {
+      width:800,
+      margin: 2,
+      color: {
+        dark: '#000000ff',
+        light:'#ffffffff'
+      }
+    }, (err, url) =>{
+      if (err) return console.error(err)
+
+      console.log(url)
+      setQrcode(url)
+    })
+  }
+
+return (
+  <div className="App">
+      <h1>Gerador de QR Code</h1>
+      <div className="btn">
+      <input type="text" placeholder="Digite a URL do site." 
+      value={url}
+      onChange={(evt) => setUrl(evt.target.value)}/>
+      <button onClick={GeradorQRcode}>Gerar QR Code</button>
+      {qrcode && <>
+        <img src={qrcode} />
+        <a href={qrcode} download="qrcode.png">Download</a>
+      </>}
+    </div>
+      </div>
+  )
+}
+
+export default App
